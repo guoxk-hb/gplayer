d2b3ce35ed725c3a65e648a951b3a6607b7b62ae
guoxk <gxk1744413631@163.com>
2025-06-06 00:41:21 +0800

    chore:  nuxt config update

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/ui'
+  ],
+  plugins: [
+    { src: '~/plugins/hls', mode: 'client' },
+    { src: '~/plugins/flv', mode: 'client' },
+    { src: '~/plugins/dash', mode: 'client' }
+  ],
+  future: {
+    compatibilityVersion: 4,
+  },
+  icon: {
+    mode: 'css',
+    cssLayer: 'base'
+  },
+})
